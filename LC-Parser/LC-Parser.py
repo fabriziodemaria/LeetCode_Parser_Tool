d1b6e96271c9821bac5aae80fadcd447e9d5c030
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2015-08-17 17:57:56 +0200

    Read arguments

--- LC-Parser/LC-Parser.py
+++ LC-Parser/LC-Parser.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+import sys
+import ast
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def generateTree(list):
+    return None
+
+list = ast.literal_eval(sys.argv[1])
+
+if __name__ == '__main__':
+    generateTree(list)
