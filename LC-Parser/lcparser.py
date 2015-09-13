eb3466653ba47b23de83d5606c9088b27c4de263
fabriziodemaria <fabrizio.f.demaria@gmail.com>
2015-09-13 15:56:32 +0200

    Simplify API

--- LC-Parser/lcparser.py
+++ LC-Parser/lcparser.py
@@ -9,32 +9,29 @@ class TreeNode:
         self.left = None
         self.right = None
 
-class TreeGenerator:
-    def __init__(self, input_string):
-        self.s = input_string
-
-    def generateTree(self):
-        if (self.s == "[]"):
-            return None
-        list = self.s[1:-1].split(',')
-        head = TreeNode(int(list[0]))
-        current = head
-        q = deque([])
-        i = 0
-        while i < len(list):
-            if i*2+1 < len(list):
-                if list[i*2+1]=="#":
-                    current.left = None
-                else:
-                    current.left = TreeNode(int(list[i*2+1]))
-                    q.append(current.left)
-            if i*2+2 < len(list):
-                if list[i*2+2]=="#":
-                    current.rightt = None
-                else:
-                    current.right = TreeNode(int(list[i*2+2]))
-                    q.append(current.right)
-            i=i+1
-            if len(q)>0:
-                current = q.popleft()
-        return head
+def TreeGenerator(string):
+    s = string
+    if (s == "[]"):
+        return None
+    list = s[1:-1].split(',')
+    head = TreeNode(int(list[0]))
+    current = head
+    q = deque([])
+    i = 0
+    while i < len(list):
+        if i*2+1 < len(list):
+            if list[i*2+1]=="#":
+                current.left = None
+            else:
+                current.left = TreeNode(int(list[i*2+1]))
+                q.append(current.left)
+        if i*2+2 < len(list):
+            if list[i*2+2]=="#":
+                current.rightt = None
+            else:
+                current.right = TreeNode(int(list[i*2+2]))
+                q.append(current.right)
+        i=i+1
+        if len(q)>0:
+            current = q.popleft()
+    return head
